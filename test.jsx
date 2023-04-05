73847eb575161f6419f7da05dae9a79e340614e2
Duduf <git@duduf.com>
2023-04-05 15:37:36 +0200

    fixed some path errors

--- test.jsx
+++ test.jsx
@@ -3,6 +3,8 @@
     #include "DuESF.jsxinc"
 
     new OCOConfig('X:/OCO.config');
-    alert(OCO.config.librarySettingsAbsoluteURI());
+    //alert(OCO.config.librarySettingsAbsoluteURI());
+
+    var file = File.saveDialog("Select the OCO.config file.", "OCO Config:*.config;YAML:*.yml,*.yaml;Text files:*.txt;All files:*.*");
 
 })(this);
