6663eb84b4cb25868d7509394fd2061536fe2eb9
Duduf <git@duduf.com>
2024-01-11 10:08:40 +0100

    Fix XMP loading fail in some cases

--- src/inc/xmp.jsx
+++ src/inc/xmp.jsx
@@ -11,13 +11,24 @@ DuXMP.initialized = false;
  * Loads the XMP library if it has not been loaded yet. There is no need to call this function as it's called automatically by DuESF methods if needed.<br />
  * Call it once if you plan to use XMP without the methods in DuESF.<br />
  * Note that the XMP library is added statically as <code>ExternalObject.AdobeXMPScript</code>.
+ * @returns {Boolean} Init may fail, in this case the function returns false and the XMP lib can't be used.
  */
 DuXMP.init = function ()
 {
-    if (DuXMP.initialized) return;
+    if (DuXMP.initialized) return true;
     
     // load the library
-    ExternalObject.AdobeXMPScript = def ( ExternalObject.AdobeXMPScript, new ExternalObject( "lib:AdobeXMPScript") );
+    try {
+    ExternalObject.AdobeXMPScript = def (
+        ExternalObject.AdobeXMPScript,
+        new ExternalObject( "lib:AdobeXMPScript" )
+        );
+    } catch (e) {
+        $.writeln("Can't load the XMP library");
+        DuXMP.initialized = false;
+        return false;
+    }
 
     DuXMP.initialized = true;
+    return true;
 }
